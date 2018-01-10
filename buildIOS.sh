3a227d38abdbb596f56a01ac5671b2c36d038dbb
CHxin <chengxinhust@163.com>
2018-01-10 16:20:53 +0800

    ios first travis with npm certification xcodebuild

--- buildIOS.sh
+++ buildIOS.sh
@@ -1,2 +1,2 @@
-xcodebuild -project "./ios/learnRN.xcodeproj" -scheme "learnRN_Test" archive -archivePath "./buildIOS/learnRN_0.0.1.xcarchive" -quiet
-xcodebuild -exportArchive -archivePath "./buildIOS/learnRN_0.0.1.xcarchive" -exportPath "./buildIOS/learnRN_0.0.1" -exportOptionsPlist "exportTestOptions.plist" -quiet
+xcodebuild -project "./ios/learnRN.xcodeproj" -scheme "learnRN" archive -archivePath "./buildIOSTemp/learnRN_0.0.1.xcarchive" -quiet
+xcodebuild -exportArchive -archivePath "./buildIOSTemp/learnRN_0.0.1.xcarchive" -exportPath "./buildIOSTemp/learnRN_0.0.1" -exportOptionsPlist "exportTestOptions.plist" -quiet
--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 import { AppRegistry } from 'react-native';
-import App from './App.js';
+import App from './navigator/reactNavigator/drawerNavigator/drawerNavigator.js';
 
 AppRegistry.registerComponent('learnRN', () => App);
