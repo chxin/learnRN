b7026ce1ac7fb28ebb440ab9f52eea3d681ba68c
CHxin <chengxinhust@163.com>
2018-01-10 22:23:08 +0800

    add certification to login keychains

--- ios/RockCer/add-key.sh
+++ ios/RockCer/add-key.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-security create-keychain -p travis ios-build.keychain
-security import ./ios_distribution.cer -k ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign
-security import ./dist.p12 -k ~/Library/Keychains/ios-build.keychain -P 123456 -T /usr/bin/codesign
+# security create-keychain -p travis ios-build.keychain
+security import ./ios_distribution.cer -k ~/Library/Keychains/login.keychain -T /usr/bin/codesign
+security import ./dist.p12 -k ~/Library/Keychains/login.keychain -P 123456 -T /usr/bin/codesign
 mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
 cp ./profile/hocEDisco.mobileprovision ~/Library/MobileDevice/Provisioning\ Profiles/
