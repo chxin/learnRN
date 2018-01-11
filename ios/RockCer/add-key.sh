dd3136f990667a343b08b670d8a6f2c07fc1b120
CHxin <chengxinhust@163.com>
2018-01-11 10:31:24 +0800

    ios-build keychain default and unlock

--- ios/RockCer/add-key.sh
+++ ios/RockCer/add-key.sh
@@ -1,6 +1,19 @@
 #!/bin/sh
-# security create-keychain -p travis ios-build.keychain
-security import ./ios_distribution.cer -k ~/Library/Keychains/login.keychain -T /usr/bin/codesign
-security import ./dist.p12 -k ~/Library/Keychains/login.keychain -P 123456 -T /usr/bin/codesign
+# security delete-keychain ios-build.keychain
+# rm -f ~/Library/MobileDevice/Provisioning\ Profiles/hocEDisco.mobileprovision
+
+security create-keychain -p travis ios-build.keychain
+security default-keychain -s ios-build.keychain
+
+security unlock-keychain -p travis ios-build.keychain
+security set-keychain-setting -t 3600 -l ~/Library/Keychains/ios-build.keychain
+
+security import ./ios_distribution.cer -k ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign
+security import ./dist.p12 -k ~/Library/Keychains/ios-build.keychain -P 123456 -T /usr/bin/codesign
+
 mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
 cp ./profile/hocEDisco.mobileprovision ~/Library/MobileDevice/Provisioning\ Profiles/
+
+security find-identity -p codesigning ~/Library/Keychains/ios-build.keychain
+security list-keychains
+
