ef0cc6b1cfeb056f519535bb4d25f225a988859b
CHxin <chengxinhust@163.com>
2018-01-12 11:09:17 +0800

    add encrypt to ios

--- ios/RockCertificate/add-key.sh
+++ ios/RockCertificate/add-key.sh
@@ -3,13 +3,13 @@
 # rm -f ~/Library/MobileDevice/Provisioning\ Profiles/hocEDisco.mobileprovision
 
 security create-keychain -p travis ios-build.keychain
-security default-keychain -s ios-build.keychain
+# security default-keychain -s ios-build.keychain
 
 security unlock-keychain -p travis ios-build.keychain
-security set-keychain-setting -t 3600 -l ~/Library/Keychains/ios-build.keychain
+# security set-keychain-setting -t 3600 -l ~/Library/Keychains/ios-build.keychain
 
-security import ./ios_distribution.cer -k ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign
-security import ./dist.p12 -k ~/Library/Keychains/ios-build.keychain -P 123456 -T /usr/bin/codesign
+security import ./ios_distribution.cer -A -k ~/Library/Keychains/ios-build.keychain -T /usr/bin/codesign
+security import ./dist.p12 -k -A ~/Library/Keychains/ios-build.keychain -P 123456 -T /usr/bin/codesign
 
 mkdir -p ~/Library/MobileDevice/Provisioning\ Profiles
 cp ./profile/hocEDisco.mobileprovision ~/Library/MobileDevice/Provisioning\ Profiles/
